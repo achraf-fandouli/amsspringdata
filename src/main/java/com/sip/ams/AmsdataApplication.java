e4bee5e76a2a0246303d02f2ca71a50839b8f850
Achraf FANDOULI <achref.fendouli@gmail.com>
2022-02-13 22:56:40 +0100

    CRUD Article et Provider : image upload

--- src/main/java/com/sip/ams/AmsdataApplication.java
+++ src/main/java/com/sip/ams/AmsdataApplication.java
@@ -1,12 +1,19 @@
 package com.sip.ams;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sip.ams.controllers.ArticleController;
+
+import java.io.File;
 @SpringBootApplication
 public class AmsdataApplication {
 
 	public static void main(String[] args) {
+		//to create the folder on project starting
+		new File(ArticleController.uploadDirectory).mkdir();
+		
 		SpringApplication.run(AmsdataApplication.class, args);
 	}
 
--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -1,6 +1,104 @@
 package com.sip.ams.controllers;
 
+
+
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sip.ams.entity.Article;
+import com.sip.ams.entity.Provider;
+import com.sip.ams.repository.ProviderRepository;
+
+@Controller
+@RequestMapping("/provider/")
+public class ProviderController {
+	private final ProviderRepository providerRepository;
+    @Autowired
+    public ProviderController(ProviderRepository providerRepository) {
+        this.providerRepository = providerRepository;
+    }
+    
+    @GetMapping("list")
+    public String listProviders(Model model) {
+    	List<Provider> lp= (List<Provider>) providerRepository.findAll();
+    	if(lp.size()==0) lp = null;
+        model.addAttribute("providers", lp);
+        return "provider/listProviders";
+    }
+    
+    @GetMapping("add")
+    public String showAddProviderForm(Provider provider) {
+        return "provider/addProvider";
+    }
+    
+    @PostMapping("add")
+    public String addProvider(@Valid Provider provider, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "provider/addProvider";
+        }
+        providerRepository.save(provider);
+        return "redirect:list";
+    }
+    
+    @GetMapping("delete/{id}")
+    public String deleteProvider(@PathVariable("id") long id, Model model) {
+        Provider provider = providerRepository.findById(id)
+            .orElseThrow(()-> new IllegalArgumentException("Invalid provider Id:" + id));
+        providerRepository.delete(provider);
+        model.addAttribute("providers", providerRepository.findAll());
+        return "provider/listProviders";
+    }
+    
+    
+    @GetMapping("edit/{id}")
+    public String showProviderFormToUpdate(@PathVariable("id") long id, Model model) {
+        Provider provider = providerRepository.findById(id)
+            .orElseThrow(()->new IllegalArgumentException("Invalid provider Id:" + id));
+        model.addAttribute("provider", provider);
+        return "provider/updateProvider";
+    }
+    @PostMapping("update/{id}")
+    public String updateProvider(@PathVariable("id") long id, @Valid Provider provider, BindingResult result,
+        Model model) {
+        if (result.hasErrors()) {
+            provider.setId(id);
+            return "provider/updateProvider";
+        }
+        providerRepository.save(provider);
+        model.addAttribute("providers", providerRepository.findAll());
+        return "provider/listProviders";
+    }
+    
+    @GetMapping("show/{id}")
+	public String showProvider(@PathVariable("id") long id, Model model) {
+		Provider provider = providerRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid provider Id:" + id));
+		List<Article> articles = providerRepository.findArticlesByProvider(id);
+		for (Article a : articles)
+			System.out.println("Article = " + a.getLabel());
+		
+		model.addAttribute("articles", articles);
+		model.addAttribute("provider", provider);
+		return "provider/showProvider";
+	}
+
+
+}
+
+
+/*
+
+ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -11,12 +109,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sip.ams.entities.Provider;
-import com.sip.ams.repositories.ProviderRepository;
+import com.sip.ams.entity.Provider;
+import com.sip.ams.repository.ProviderRepository;
 
 import java.util.List;
 
 import javax.validation.Valid;
+
+
 @Controller
 @RequestMapping("/provider/")
 public class ProviderController {
@@ -75,8 +175,9 @@ System.out.println("suite du programme...");
         
         providerRepository.delete(provider);
         
-        /*model.addAttribute("providers", providerRepository.findAll());
-        return "provider/listProviders";*/
+        //model.addAttribute("providers", providerRepository.findAll());
+        //return "provider/listProviders";
+
         //../list : il va monter d'un niveau
         return "redirect:../list";
     }
@@ -104,4 +205,4 @@ System.out.println("suite du programme...");
     }
 }
 
-
+*/
